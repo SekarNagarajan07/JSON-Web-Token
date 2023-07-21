60b375c08c13461ab12037abbcd789dd070d374f
sekar N <sekarnagarajan1234@gmail.com>
2023-07-21 11:20:19 +0530

    js

--- src/index.js
+++ src/index.js
@@ -27,3 +27,16 @@ server.use(express.urlencoded({ extended: true })); // support url-encoded body
 server.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
+
+// 1. Register a user
+
+server.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // 1. check if user exist
+
+    const hashedPassword = await hash(password, 10);
+    console.log(hashedPassword);
+  } catch (error) {}
+});
