e0c6c1eac383deeefe959b42734d5fbf344a29ad
sekar N <sekarnagarajan1234@gmail.com>
2023-07-21 11:05:42 +0530

    js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ const cookieparser = require("cookie-parser");
 const cors = require("cors");
 const { verify } = require("jsonwebtoken");
 const { hash, compare } = require("bcryptjs");
+
+const server = express();
+
+// use express middleware for easier cookie handling
+
+server.use(cookieparser());
+
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
